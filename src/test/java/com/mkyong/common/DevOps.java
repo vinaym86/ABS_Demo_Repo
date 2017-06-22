ba5f3756010cf31035085cc63b323f1e704cdf38
vinaym86 <vinaymrao@hotmail.com>
2017-06-22 18:49:05 +0530

    Update DevOps.java

--- src/test/java/com/mkyong/common/DevOps.java
+++ src/test/java/com/mkyong/common/DevOps.java
@@ -1,23 +1,23 @@
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-
-
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DevOps {
 	public WebDriver driver;
 	
   
   public void setup() throws InterruptedException {
-	 // System.setProperty("webdriver.ie.driver", "D:\\vk00418361\\IEDriverServer.exe"); //sets the system property to the IE driver. Download from( http://www.seleniumhq.org/download/)
-	  /*driver = new FirefoxDriver();
+	 System.setProperty("webdriver.chrome.driver", "./src/main/resources/chromedriver"); //sets the system property to the IE driver. Download from( http://www.seleniumhq.org/download/)
+	 driver = new ChromeDriver();
 	  driver.manage().window().maximize(); //Maximize the window
 	  driver.get("https://www.google.co.in"); //enters this URL and navigates
 	  System.out.println("Browser Launched");
 	  Thread.sleep(5000); //waits for 10 seconds
-  	  System.out.println("Setup Completed and title");  */	    
-WebDriver DRIVER = new FirefoxDriver();
-DRIVER.get("http://www.google.com");	  
+  	  System.out.println("Setup Completed and title");  	    
+/*WebDriver DRIVER = new ChromeDriver();
+DRIVER.get("http://www.google.com");	  */
 	  
   }
 	
