dc21b5d2f1c760c4c50165bf99a7df4e5328c878
vinaym86 <vinaymrao@hotmail.com>
2017-06-22 19:25:11 +0530

    Update DevOps.java

--- src/test/java/com/mkyong/common/DevOps.java
+++ src/test/java/com/mkyong/common/DevOps.java
@@ -9,15 +9,16 @@ public class DevOps {
 	
   
   public void setup() throws InterruptedException {
-	// System.setProperty("webdriver.chrome.driver", "./src/main/resources/chromedriver"); //sets the system property to the IE driver. Download from( http://www.seleniumhq.org/download/)
-	 driver = new FirefoxDriver();
+	/* System.setProperty("webdriver.chrome.driver", "./src/main/resources/chromedriver"); //sets the system property to the IE driver. Download from( http://www.seleniumhq.org/download/)
+	 
 	  driver.manage().window().maximize(); //Maximize the window
 	  driver.get("https://www.google.co.in"); //enters this URL and navigates
 	  System.out.println("Browser Launched");
 	  Thread.sleep(5000); //waits for 10 seconds
-  	  System.out.println("Setup Completed and title");  	    
-/*WebDriver DRIVER = new ChromeDriver();
-DRIVER.get("http://www.google.com");	  */
+  	  System.out.println("Setup Completed and title");  	*/    
+//WebDriver DRIVER = new ChromeDriver();
+	  driver = new FirefoxDriver();
+driver.get("http://www.google.com");	  
 	  
   }
 	
